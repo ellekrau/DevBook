ed9aa0489c610f04fd45feb426710d8001b88f29
ellekrau <ellekrau91@gmail.com>
2022-03-04 01:08:04 -0300

    Configure user routes

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represent all api routes
 type Route struct {
@@ -8,4 +12,12 @@ type Route struct {
 	Method                 string
 	Function               func(http.ResponseWriter, *http.Request)
 	RequiresAuthentication bool
+}
+
+func Configure(router *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		router.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
 }
--- api/src/router/routes/users.go
+++ api/src/router/routes/users.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"api/src/controllers"
 	"fmt"
 	"net/http"
 )
@@ -9,15 +10,9 @@ var uriBase string = "/users"
 var uriWithId string = fmt.Sprintf("%s/{id}", uriBase)
 
 var userRoutes = []Route{
-	{URI: uriBase, Method: http.MethodPost, Function: create, RequiresAuthentication: false},
-	{URI: uriBase, Method: http.MethodGet, Function: getAll, RequiresAuthentication: false},
-	{URI: uriWithId, Method: http.MethodGet, Function: getById, RequiresAuthentication: false},
-	{URI: uriWithId, Method: http.MethodPost, Function: update, RequiresAuthentication: false},
-	{URI: uriWithId, Method: http.MethodDelete, Function: delete, RequiresAuthentication: false},
+	{URI: uriBase, Method: http.MethodPost, Function: controllers.CreateUser, RequiresAuthentication: false},
+	{URI: uriBase, Method: http.MethodGet, Function: controllers.GetAllUsers, RequiresAuthentication: false},
+	{URI: uriWithId, Method: http.MethodGet, Function: controllers.GetUserById, RequiresAuthentication: false},
+	{URI: uriWithId, Method: http.MethodPost, Function: controllers.UpdateUser, RequiresAuthentication: false},
+	{URI: uriWithId, Method: http.MethodDelete, Function: controllers.DeleteUser, RequiresAuthentication: false},
 }
-
-func create(rw http.ResponseWriter, r *http.Request)  {}
-func getAll(rw http.ResponseWriter, r *http.Request)  {}
-func getById(rw http.ResponseWriter, r *http.Request) {}
-func update(rw http.ResponseWriter, r *http.Request)  {}
-func delete(rw http.ResponseWriter, r *http.Request)  {}

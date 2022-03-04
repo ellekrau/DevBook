5459f34ae621d1009ba3deacf18c9b9260447ccb
ellekrau <ellekrau91@gmail.com>
2022-03-04 01:00:27 -0300

    Add user controller method comments

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -2,22 +2,27 @@ package controllers
 
 import "net/http"
 
+// CreateUser create a new user in database
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusNotImplemented)
 }
 
+// GetAllUsers get all users from database
 func GetAllUsers(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusNotImplemented)
 }
 
+// GetUserById use one ID to get an user from database
 func GetUserById(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusNotImplemented)
 }
 
+// UpdateUser update an user from database
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusNotImplemented)
 }
 
+// DeleteUser delete an user from database
 func DeleteUser(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusNotImplemented)
 }
